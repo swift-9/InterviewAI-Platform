43d1c0ccffb669af07f289fddc665b1a6d4a863f
Swastik Poojary <swastikjayaraj500@gmail.com>
2025-07-15 12:28:55 +0530

    Implement interview generation page ui

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,5 +1,11 @@
-const page = () => {
-  return <div>page</div>;
-};
+import Agent from "@/components/agent";
 
+export const page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName="You" userId="user1" type="generated" />
+    </>
+  );
+};
 export default page;
---
+++ components/agent.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
+
+const Agent = ({ userName }: AgentProps) => {
+  const isSpeaking = true;
+  const callStatus = CallStatus.ACTIVE;
+  const messages: SavedMessage[] = [
+    { role: "user", content: "Hello, how are you?" },
+    { role: "assistant", content: "I'm doing well, thank you!" },
+  ];
+  const lastMessage = messages[messages.length - 1]?.content || "";
+  return (
+    <>
+      <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
+        </div>
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call" onClick={() => handleCall()}>
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
+            End
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Agent;
